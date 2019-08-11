8568b638c0f963efdfe7a3e27bc669cfb35628bb
Nisha Kunhikrishnan <nishakk94@gmail.com>
2019-08-11 15:51:51 -0700

    Knapsack

---
+++ Knapsack.java
@@ -0,0 +1,53 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Knapsack {
+	public static void main(String[] args) {
+		int[] w = {10, 20, 30};
+		int[] v = {60, 100, 120};
+		System.out.println(knapsack(50, w, v, 0));
+		System.out.println(knapsack_cache(50, w, v, 0, new HashMap<Integer, Map<Integer, Integer>>()));
+		System.out.println(knapsack_dp(50, w, v));
+	}
+	
+	/**
+	 * Naive Solution
+	 */
+	private static int knapsack(int W, int[] weights, int[] vals, int i) {
+		if (i == weights.length) return 0;
+		if (W - weights[i] < 0) return knapsack(W, weights, vals, i + 1);
+		return Math.max(knapsack(W - weights[i], weights, vals, i + 1) + vals[i], knapsack(W, weights, vals, i + 1));
+	}
+	
+	/**
+	 * Recursive solution with caching to improve performance
+	 */
+	private static int knapsack_cache(int W, int[] weights, int[] vals, int i, Map<Integer, Map<Integer, Integer>> cache) {
+		if (i == weights.length) return 0;
+		
+		if (!cache.containsKey(i)) cache.put(i, new HashMap<>());
+		Integer cached = cache.get(i).get(W);
+	    if (cached != null) return cached;
+	    
+	    if (W - weights[i] < 0) return knapsack(W, weights, vals, i + 1);
+		
+		int val = Math.max(knapsack_cache(W - weights[i], weights, vals, i + 1, cache) + vals[i], 
+				knapsack_cache(W, weights, vals, i + 1, cache));
+		cache.get(i).put(W, val);
+		return val;
+	}
+	
+	/**
+	 * DP Solution time complexity: O(n * W)
+	 */
+	private static int knapsack_dp(int W, int[] weights, int[] vals) {
+		int[][] cache = new int[weights.length + 1][W + 1];
+		for (int i = 1; i <= weights.length; i++) {
+			for (int j = 1; j <= W; j++) {
+				if (weights[i - 1] > j) cache[i][j] = cache[i - 1][j];
+				else cache[i][j] = Math.max(cache[i - 1][j], cache[i - 1][j - weights[i - 1]] + vals[i - 1]);
+			}
+		}
+		return cache[weights.length][W];
+	}
+}
--- MedianOfArrays.java
+++ MedianOfArrays.java
@@ -8,12 +8,11 @@ public class MedianOfArrays {
 	public static void main(String[] args) {
 		int[] arr1 = {1, 3, 5};
 		int[] arr2 = {2, 4, 6};
-		System.out.println(medianOfArrays(arr1, arr2));
+		System.out.println(findMedianSortedArraysBinarySearch(arr1, arr2));
 	}
 	
 	/**
 	 * Solution Time Complexity: O(n)
-	 * Better Solution using Binary Search: O(log n)
 	 */
 	private static float medianOfArrays(int[] a1, int[] a2) {
 		float[] arr = new float[a1.length + a2.length];
@@ -43,5 +42,42 @@ public class MedianOfArrays {
 			return (arr[medIdx - 1] + arr[medIdx]) / 2;
 		}
 		return arr[medIdx];
+	}
+	
+	/**
+	 * Binary Search Solution Time Complexity: O(log(min(m,n)))
+	 */
+	private static double findMedianSortedArraysBinarySearch(int[] nums1, int[] nums2) {
+		if (nums1.length > nums2.length) {
+			return findMedianSortedArraysBinarySearch(nums2, nums1);
+		}
+		
+		int m = nums1.length;
+		int n = nums2.length;
+		int lo = 0;
+		int hi = m;
+		while (lo <= hi) {
+			int i = lo + (hi - lo) / 2;
+			int j = (m + n + 1) / 2 - i;
+			int maxLeftA = i == 0 ? Integer.MIN_VALUE : nums1[i - 1];
+			int minRightA = i == m ? Integer.MAX_VALUE : nums1[i];
+			
+			int maxLeftB = j == 0 ? Integer.MIN_VALUE : nums2[j - 1];
+			int minRightB = j == n ? Integer.MAX_VALUE : nums2[j];
+			
+			if (maxLeftA <= minRightB && maxLeftB >= minRightA) {
+				if ((m + n) % 2 == 0) {
+					return (double)(Math.max(maxLeftA, maxLeftB) + Math.min(minRightA, minRightB)) / 2;
+				} else {
+					return (double)(Math.max(maxLeftA, maxLeftB));
+				}
+			} else if (maxLeftA > minRightB) {
+				hi = i - 1;
+			} else {
+				lo = i + 1;
+			}
+		}
+		
+		return 0.0;
 	}
 }
